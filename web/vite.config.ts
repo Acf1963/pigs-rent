1da6db454a0c5aa1c3b08cfe922c9ffa5123a5c8
Acf1963 <antoniocarlos.fernandes@hot.mail.com>
2026-03-24 11:47:38 +0100

    update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,11 @@
-// web/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Garante que os assets sejam buscados na raiz do domínio
+  base: '', // <-- Crítico para resolver o erro de MIME type
   build: {
     outDir: 'dist',
+    emptyOutDir: true
   }
 })
