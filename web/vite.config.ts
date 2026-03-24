6267436e82c77713b0d81670555ec2a51ab755c4
Acf1963 <antoniocarlos.fernandes@hot.mail.com>
2026-03-24 12:11:30 +0100

    update vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Força caminhos relativos para os scripts
+  base: './', // <-- MUITO IMPORTANTE: Resolve o erro de MIME type em subpastas
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     emptyOutDir: true
   }
 })
