b3f7ac7f27f529a9f5cc6d67f1245d4237099d01
Acf1963 <antoniocarlos.fernandes@hot.mail.com>
2026-03-24 12:17:04 +0100

    update Vercel config and Vite base path to fix MIME type errors

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- MUITO IMPORTANTE: Resolve o erro de MIME type em subpastas
+  base: '', // <-- Isto remove a barra inicial e resolve o erro de MIME type
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
